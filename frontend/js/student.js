846958de388318abb1560775b75f7fe80b47105e
AR2908 <162954754+AR2908@users.noreply.github.com>
2026-04-05 16:45:09 +0530

    Update student.js

--- frontend/js/student.js
+++ frontend/js/student.js
@@ -104,7 +104,7 @@ document.getElementById('applyForm').addEventListener('submit', async (e) => {
     const leaveDate = document.getElementById('leaveDate').value;
     const returnDate = document.getElementById('returnDate').value;
 
-    const res = await fetch('${API_BASE_URL}/api/student/apply', {
+    const response = await fetch(`${API_BASE_URL}/api/student/apply`,, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ studentId: user._id, reason, leaveDate, returnDate })
