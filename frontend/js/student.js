14fee7d888f41976210f4dd1b4d1735fb7ac75b5
AR2908 <162954754+AR2908@users.noreply.github.com>
2026-04-05 18:45:49 +0530

    Update student.js

--- frontend/js/student.js
+++ frontend/js/student.js
@@ -82,8 +82,9 @@ async function loadMyHistory() {
             if (h.entryTime) {
                 const entryRow = `
                     <tr style="background-color: #f0fff4;">
-                        <td>${leaveDateStr}</td>
                         <td>${h.reason}</td>
+                        <td>${leaveDateStr}</td>
+                        
                         <td><b style="color: #27ae60">COMPLETED</b></td>
                         <td><b style="color:#27ae60">IN ✅</b><br><small>${formatDateTime(h.entryTime)}</small></td>
                     </tr>`;
@@ -95,8 +96,9 @@ async function loadMyHistory() {
             if (h.exitTime) {
                 const exitRow = `
                     <tr style="background-color: #fff5f5;">
+                    <td>${h.reason}</td>
                         <td>${leaveDateStr}</td>
-                        <td>${h.reason}</td>
+                        
                         <td><b style="color: ${statusColor}">${h.status}</b></td>
                         <td><b style="color:#d63031">OUT 🚩</b><br><small>${formatDateTime(h.exitTime)}</small></td>
                     </tr>`;
