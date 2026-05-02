4799d51f33be8dde684568dc88b3a988f0a400c7
AR2908 <162954754+AR2908@users.noreply.github.com>
2026-05-02 16:09:25 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -203,7 +203,7 @@ if (registerForm) {
             const data = await res.json();
 
             if (res.ok) {
-                alert("✅ Registration Successful!\nYour request has been sent to the Admin. You can login once your details are veridied.");
+                alert("✅ Registration Successful!\nYour request has been sent to the Admin. You can login once your details are verified");
                 registerForm.reset();
                 toggleForms('login'); // Form submit hone ke baad wapas login page dikhao
             } else {
